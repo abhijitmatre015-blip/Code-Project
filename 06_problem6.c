67fd656649cf7864e7c15749571aedb7cd85e3ab
abhijitmatre015-blip <abhijitmatre015@gmail.com>
2026-05-05 22:11:54 +0530

    Add files via upload

---
+++ 06_problem6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int n=1;
+    int prime = 1;
+    if (n == 0 || n == 1)
+    {
+        prime = 0;
+    }
+    else
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+    }
+    if (prime)
+    {
+        printf("%d is a prime number\n", n);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", n);
+    }
+
+    return 0;
+}
